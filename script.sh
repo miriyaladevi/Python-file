a2162eca2944f3d57de94018c4a55ad36c663554
miriyaladevi <113349770+miriyaladevi@users.noreply.github.com>
2023-02-06 14:10:35 +0530

    script.sh

---
+++ script.sh
@@ -0,0 +1,9 @@
+git clone https://github.com/Maddalarajesh/Agri.git
+cd Agri
+pip3 install -r requirements.txt
+screen -m -d python3 app.py
+
+git clone https://github.com/Maddalarajesh/Fuel-Consumption-Rating.git
+cd Fuel-Consumption-Rating
+pip3 install -r requirements.txt
+screen -m -d python3 app.py
