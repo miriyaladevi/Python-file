facf211d41f6dbc441b0fdbeedad9cb76aa8243e
miriyaladevi <113349770+miriyaladevi@users.noreply.github.com>
2023-02-06 16:33:15 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -7,3 +7,8 @@ git clone https://github.com/Maddalarajesh/Fuel-Consumption-Rating.git
 cd Fuel-Consumption-Rating
 pip3 install -r requirements.txt
 screen -m -d python3 app.py
+
+git clone https://github.com/miriyaladevi/USA-Housing.git
+cd USA-Housing
+pip3 insall -r reuirements
+screen -m -d python3 app.py
