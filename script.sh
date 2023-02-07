bf9d1fc1a1f5f066cf4b02ec6df7a3f1ac1353cb
miriyaladevi <113349770+miriyaladevi@users.noreply.github.com>
2023-02-07 14:23:02 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
 sudo apt update
 sudo apt-get full-upgrade -y
-sudo apt-get install python3-pip
+sudo apt-get install python3-pip -y
 git clone https://github.com/Maddalarajesh/Agri.git
 cd Agri
 pip3 install -r requirements.txt
