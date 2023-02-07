eefe4e27bf22202133f813ea937eced6cb34b734
miriyaladevi <113349770+miriyaladevi@users.noreply.github.com>
2023-02-07 22:19:09 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -4,12 +4,13 @@ sudo apt-get install python3-pip -y
 git clone https://github.com/miriyaladevi/Agri.git
 cd Agri
 pip3 install -r requirements.txt
-python3 app.py
+screen -m -d python3 app.py
+
 
 git clone https://github.com/miriyaladevi/Fuel-Consumption-Rating12.git
 cd Fuel-Consumption-Rating12
 pip3 install -r requirements.txt
-screen -m -d python3 app.py
+python3 app.py
 
 git clone https://github.com/miriyaladevi/USA-Housing.git
 cd USA-Housing
